753e00fa239f9918df75e7e50934837f545736bd
JoseVinicius05 <joseviniciusgrecco05@gmail.COM>
2023-12-11 23:01:22 -0300

    fotoMaria e css organizado

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const aaa = document.querySelector(".barras");
+const barras_menu = document.querySelector(".barras");
 const navMenu = document.querySelector(".menu_Nav");
 
-aaa.addEventListener("click", ()=>{
-    aaa.classList.toggle('active');
+barras_menu.addEventListener("click", ()=>{
+    barras_menu.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
 function navigateTo(page) {
     window.location.href = page;
   }
-//teste
